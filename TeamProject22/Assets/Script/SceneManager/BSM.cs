0ae303c977c0580ec90639796351ee8e64a8112c
kimjaehui02 <momg4287@naver.com>
2022-06-16 20:11:16 +0900

    백업용

---
+++ TeamProject22/Assets/Script/SceneManager/BSM.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSM : MonoBehaviour
+{
+
+    public GameObject[] effect;
+    public GameObject[] Enemy;
+
+}
