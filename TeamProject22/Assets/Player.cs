063ffa7fb2efd09366efd388e34476082002619f
kimjaehui02 <momg4287@naver.com>
2022-06-16 18:11:28 +0900

    플레이어

--- TeamProject22/Assets/Player.cs
+++ TeamProject22/Assets/Player.cs
@@ -7,12 +7,40 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    //          Default
+    #region         Default
+
+    //          Public
+    #region public
+    #endregion public
+
+    //          Private
+    #region private
+
+
+
+    #endregion private
+
+    #endregion      Default
+
+    // Custom
+    #region Custom      
+
+    #region public
+    #endregion
+
+    #region private
+    #endregion
+
+    #endregion
+
 }
